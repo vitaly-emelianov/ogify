92842659d51066ffdcdd9eceace95b639acb3eb1
Yanina Anastasia <ayanina@mail.ru>
2015-10-09 13:27:15 +0400

    DCS-234 minor syntax changes

--- data_analysis/build_docwords_vocab.py
+++ data_analysis/build_docwords_vocab.py
@@ -17,46 +17,46 @@ def get_word_index(w, word_index, vocab_words):
     return word_index[w]
         
 def read_posts(token_path):
-	posts = []  
+    posts = []  
     with open(token_path, 'r') as post_output:
         for line in post_output:
             if len(line.split(' ')) >= 2:
                 posts.append(line.split(' ')[1])
-	return posts
+    return posts
 	
 def build_docwords(docwords_tail_path, docwords_head_path, posts):
-	n_words = 0
-	n_entries = 0
-	vocab_words = {}
-	with open(docwords_tail_path, 'w') as f_docwords_tail:
-		for n_doc, post in enumerate(posts):    
+    n_words = 0
+    n_entries = 0
+    vocab_words = {}
+    with open(docwords_tail_path, 'w') as f_docwords_tail:
+	for n_doc, post in enumerate(posts):    
 
-			if n_doc % 1000 == 0:
-				print u'%d post processed' % (n_doc)
+		if n_doc % 1000 == 0:
+			print u'%d post processed' % (n_doc)
 
-			bag_of_words = collections.Counter()
+		bag_of_words = collections.Counter()
 
-			for word in post.split(',')[:-1]:
-				#not necessary when preprocessing was done
-				#word = word.lower().replace(u'¸', u'å')
-				if word != '':
-					word_idx = get_word_index(word, word_index, vocab_words)
-					bag_of_words[word_idx] += 1
+		for word in post.split(',')[:-1]:
+			#not necessary when preprocessing was done
+			#word = word.lower().replace(u'¸', u'å')
+			if word != '':
+				word_idx = get_word_index(word, word_index, vocab_words)
+				bag_of_words[word_idx] += 1
 
-			for word_idx, count in sorted(bag_of_words.iteritems(), key=lambda r: r[0]):
-				print >>f_docwords_tail, n_doc+1, word_idx+1, count
-				n_entries += 1
+		for word_idx, count in sorted(bag_of_words.iteritems(), key=lambda r: r[0]):
+			print >>f_docwords_tail, n_doc+1, word_idx+1, count
+			n_entries += 1
 
-			if n_doc >= MAX_DOCS:
-				break
+		if n_doc >= MAX_DOCS:
+			break
 
-	with open(docwords_head_path, 'w') as f:
-		print >>f, n_doc, n_words, n_entries
-		
-	return vocab_words
+    with open(docwords_head_path, 'w') as f:
+	print >>f, n_doc, n_words, n_entries
+	
+    return vocab_words
 		
 def build_vocab(vocab_path, vocab_words):
-	with open(vocab_path, 'w') as f:
+    with open(vocab_path, 'w') as f:
 		for word_idx in xrange(n_words):
 			print >>f, vocab_words[word_idx]
 	
@@ -67,12 +67,12 @@ if __name__ == "__main__":
     docwords_head_path = sys.argv[3] 
     vocab_path = sys.argv[4]
 	
-	word_index = {}
-	n_words = 0
-	n_entries = 0
-	vocab_words = {}
+    word_index = {}
+    n_words = 0
+    n_entries = 0
+    vocab_words = {}
 
     posts = read_posts(token_path)
 
-	vocab_words = build_docwords(docwords_tail_path, docwords_head_path, posts)
-	build_vocab(vocab_path, vocab_words)
+    vocab_words = build_docwords(docwords_tail_path, docwords_head_path, posts)
+    build_vocab(vocab_path, vocab_words)
