a1fd9bd64856b90faf1edeb8b67e25b09ecd8f9c
Morgen Matvey <melges.morgen@gmail.com>
2015-10-15 02:48:13 +0300

    Use sort by time.

--- srv/src/main/java/net/ogify/database/entities/Order.java
+++ srv/src/main/java/net/ogify/database/entities/Order.java
@@ -40,8 +40,7 @@ import java.util.List;
                     "or (orders.namespace = :enumOrderPrivate and orders.executor = :user)" +
                 ") " +
                 "and orders.owner != :user " +
-                "ORDER BY ABS(orders.longitude - :longitude) + ABS(orders.latitude - :latitude)" +
-                ""),
+                "ORDER BY orders.expireIn"),
         @NamedQuery(name = "Order.getOrderByIdFiltered", query = "SELECT orders FROM Order orders WHERE " +
                     "orders.owner = :user AND orders.id = :orderId " +
                 "UNION SELECT orders FROM Order orders, User owners WHERE " +
