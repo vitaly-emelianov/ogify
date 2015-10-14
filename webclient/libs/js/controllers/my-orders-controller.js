1f19f43ef6fe2c22bcf91894c63ff21e1a57ef6a
EmelianovVitaly <vitalik.emelianov@gmail.com>
2015-10-14 18:54:35 +0300

    [DCS-250] Header deleted, mobile devices poor displaying bug fixed.

--- webclient/libs/js/controllers/my-orders-controller.js
+++ webclient/libs/js/controllers/my-orders-controller.js
@@ -1,7 +1,8 @@
 ogifyApp.controller('MyOrdersController', function ($scope, Order, ClickedOrder) {
 
     $scope.myOrders = Order.getMyOrders();
-    $scope.maxDescriptionLength = 20;
+    $scope.maxDescriptionLength = 50;
+    $scope.maxAddressLength = 20;
 
     $scope.setClickedOrder = function(order) {
         ClickedOrder.set(order);
@@ -18,5 +19,9 @@ ogifyApp.controller('MyOrdersController', function ($scope, Order, ClickedOrder)
     $scope.onlyDone = function(order) {
         return order.status == 'Done';
     }
+
+    $scope.$on('createdNewOrderEvent', function(event, order) {
+        $scope.myOrders.push(order);
+    });
 
 });
