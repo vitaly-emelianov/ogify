ec6a5e4daa480e79742f1e658bccfc6d6151c819
EmelianovVitaly <vitalik.emelianov@gmail.com>
2015-10-17 13:14:01 +0300

    [DCS-250] Orders displaying bug fixed.

--- webclient/libs/js/controllers/my-orders-controller.js
+++ webclient/libs/js/controllers/my-orders-controller.js
@@ -1,6 +1,8 @@
-ogifyApp.controller('MyOrdersController', function ($scope, Order, ClickedOrder) {
+ogifyApp.controller('MyOrdersController', function ($scope, UserProfile, Order, ClickedOrder) {
 
+    $scope.user = UserProfile.get();
     $scope.myOrders = Order.getMyOrders();
+
     $scope.maxDescriptionLength = 50;
     $scope.maxAddressLength = 20;
 
@@ -8,20 +10,25 @@ ogifyApp.controller('MyOrdersController', function ($scope, Order, ClickedOrder)
         ClickedOrder.set(order);
     };
 
+    $scope.createdByMe = function(order) {
+        return order.owner.userId == $scope.user.userId;
+    }
+
     $scope.onlyNew = function(order) {
         return order.status == 'New';
-    }
+    };
 
     $scope.onlyRunning = function(order) {
         return order.status == 'Running';
-    }
+    };
 
     $scope.onlyDone = function(order) {
         return order.status == 'Done';
-    }
+    };
 
     $scope.$on('createdNewOrderEvent', function(event, order) {
         $scope.myOrders.push(order);
     });
 
 });
+
