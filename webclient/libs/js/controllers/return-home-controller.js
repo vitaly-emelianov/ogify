f9e0f3fe6ed33eeb45dc8cf7d2f6e58fe1cd3517
EmelianovVitaly <vitalik.emelianov@gmail.com>
2015-10-25 19:30:43 +0300

    [DCS-313] Return home button implemented.

---
+++ webclient/libs/js/controllers/return-home-controller.js
@@ -0,0 +1,14 @@
+ogifyApp.controller('ReturnHomeController', function ($rootScope, $scope, uiGmapGoogleMapApi) {
+
+    $scope.returnHome = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                $rootScope.map.center = {latitude: position.coords.latitude, longitude: position.coords.longitude};
+                $rootScope.selfMarker.coords = {latitude: position.coords.latitude, longitude: position.coords.longitude};
+                $rootScope.$apply();
+            });
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    };
+});
