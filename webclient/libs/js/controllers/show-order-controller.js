7b20ada7dda36568abe4770c4145acf62f874015
Morgen Matvey <melges.morgen@gmail.com>
2015-11-22 15:06:29 +0300

    [DCS-311] Move current time directive to separate file.

--- webclient/libs/js/controllers/show-order-controller.js
+++ webclient/libs/js/controllers/show-order-controller.js
@@ -84,28 +84,4 @@ ogifyApp.controller('ShowOrderModalController', function ($scope, $rootScope, $f
     $scope.getExpireTime = function() {
         return $filter('date')(ClickedOrder.order.expireIn, 'HH:mm');
     };
-}).directive('myCurrentTime', ['$interval', 'dateFilter',
-    function($interval, dateFilter) {
-        // return the directive link function. (compile function not needed)
-        return function(scope, element, attrs) {
-            var stopTime; // so that we can cancel the time updates
-
-            // used to update the UI
-            function updateTime() {
-                element.text(dateFilter(new Date()));
-            }
-
-            // watch the expression, and update the UI on change.
-            scope.$watch(attrs.myCurrentTime, function(value) {
-                updateTime();
-            });
-
-            stopTime = $interval(updateTime, 1000);
-
-            // listen on DOM destroy (removal) event, and cancel the next UI update
-            // to prevent updating time after the DOM element was removed.
-            element.on('$destroy', function() {
-                $interval.cancel(stopTime);
-            });
-        }
-    }]);
+});
---
+++ webclient/libs/js/directives/current-time-directive.js
@@ -0,0 +1,29 @@
+/**
+ * Created by melge on 22.11.2015.
+ */
+ogifyApp.directive('myCurrentTime', ['$interval', 'dateFilter',
+    function ($interval, dateFilter) {
+        // return the directive link function. (compile function not needed)
+        return function (scope, element, attrs) {
+            var stopTime; // so that we can cancel the time updates
+
+            // used to update the UI
+            function updateTime() {
+                element.text(dateFilter(new Date()));
+            }
+
+            // watch the expression, and update the UI on change.
+            scope.$watch(attrs.myCurrentTime, function (value) {
+                updateTime();
+            });
+
+            stopTime = $interval(updateTime, 1000);
+
+            // listen on DOM destroy (removal) event, and cancel the next UI update
+            // to prevent updating time after the DOM element was removed.
+            element.on('$destroy', function () {
+                $interval.cancel(stopTime);
+            });
+        }
+    }
+]);
